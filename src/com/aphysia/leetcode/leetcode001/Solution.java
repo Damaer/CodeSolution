1c36201db60d23ad4c6b3445531b61651ec1e19d
Damaer <976170778@qq.com>
2020-10-23 23:00:40 +0800

    01leetcode.md

--- src/com/aphysia/leetcode/leetcode001/Solution.java
+++ src/com/aphysia/leetcode/leetcode001/Solution.java
@@ -1,4 +1,38 @@
-package com.aphysia.leetcode.leetcode01;
+package com.aphysia.leetcode.leetcode001;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[2];
+        }
+        int[] result = new int[2];
+        Map<Integer, Integer> numSet = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numSet.containsKey(target - nums[i])) {
+                result[0] = numSet.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            } else {
+                numSet.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        int []result = new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=1;i+j<nums.length;j++){
+                if(nums[i]+nums[i+j]==target){
+                    result[0]=i;
+                    result[1]=i+j;
+                }
+            }
+        }
+        return result;
+    }
 }
