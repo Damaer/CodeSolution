05de7cd324798c73019a06348fa92a18373d3f0b
wenhaoxu <wenhaoxu@trip.com>
2021-02-01 22:04:16 +0800

    增加文件

---
+++ Solution/src/com/aphysia/offer/Solution41.java
@@ -0,0 +1,34 @@
+package com.aphysia.offer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wenhaoxu
+ * @date 2021/2/1 21:54
+ */
+public class Solution41 {
+
+    public static void main(String[] args) {
+        Solution41 solution41 = new Solution41();
+        System.out.println(solution41.FindContinuousSequence(3));
+    }
+
+    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> results= new ArrayList<>();
+        for(int start = 1;start<=sum/2;start++){
+            int tempSum = 0;
+            ArrayList<Integer> integers = new ArrayList<>();
+            for (int j=start;j<sum;j++){
+                integers.add(j);
+                tempSum = tempSum+j;
+                if(tempSum==sum){
+                    results.add(integers);
+                }else if(tempSum>sum){
+                    continue;
+                }
+            }
+        }
+        return results;
+    }
+}
